b7cecc06b4ca65d6380552a49cd4459ed8395d6a
Jan Wozniak <jawoznia@cisco.com>
2018-08-16 14:03:05 +0200

    cleaner output

--- scripts/es_pvc_resize.sh
+++ scripts/es_pvc_resize.sh
@@ -18,53 +18,64 @@ set -e
 
 # Set StatefulSet name
 SS=jw5-pvc-debug
+NS=jwtest
 
 # Set post resize capacity for PVs
-CAPACITY=3Gi
+CAPACITY=15Gi
 
-selector=(`kubectl get statefulset $SS -o go-template='{{range $k, $v := .spec.selector.matchLabels}}--selector={{$k}}={{$v}} {{end}}'`)
-pods=(`kubectl get pod $selector -o go-template='{{range .items}}{{.metadata.name}} {{end}}'`)
-pvcs=(`kubectl get pod $selector -o go-template='{{range .items}}{{range .spec.volumes}}{{if eq .name "data"}}{{.persistentVolumeClaim.claimName}} {{end}}{{end}}{{end}}'`)
+selector=`kubectl get statefulset --namespace=$NS $SS -o go-template='--selector={{range $k, $v := .spec.selector.matchLabels}}{{$k}}={{$v}},{{end}}' | sed 's/,$//'`
+pods=(`kubectl get pod --namespace=$NS $selector -o go-template='{{range .items}}{{.metadata.name}} {{end}}'`)
+pvcs=(`kubectl get pod --namespace=$NS $selector -o go-template='{{range .items}}{{range .spec.volumes}}{{if eq .name "data"}}{{.persistentVolumeClaim.claimName}} {{end}}{{end}}{{end}}'`)
 
 for pvc in ${pvcs[@]}; do
     echo "patch pvc $pvc"
-    kubectl patch pvc $pvc --patch='{"spec":{"resources":{"requests":{"storage":"'$CAPACITY'"}}}}'
+    kubectl patch pvc --namespace=$NS $pvc --patch='{"spec":{"resources":{"requests":{"storage":"'$CAPACITY'"}}}}'
 done
 
 echo "scale down ss $SS"
-kubectl scale statefulset $SS --replicas=0
+kubectl scale statefulset --namespace=$NS $SS --replicas=0
 
-echo "killing a pod ${pods[@]}"
-kubectl delete pod ${pods[@]} --grace-period=0
-# for each existing pod
-#   if in terminating status
-#     if exec to the pod
-#       while ! kill preStop hook
-#       mark as killed
-#     else
-#       mark as killed
+echo ""
+while [[ ! -z `kubectl get pod --namespace=$NS $selector -o go-template='{{range .items}}{{.metadata.name}} {{end}}'` ]]; do
+    terminating=(`kubectl get pod --namespace=$NS $selector | awk '/Terminating/{print($1)}'`)
+    for pod in ${terminating[@]}; do
+        echo -ne "\e[0K\rterminating pod $pod"
+        pid=`kubectl exec --namespace=$NS $pod -- ps aux | awk '/pre-stop-hook.sh/{print($2)}'`
+        if [[ ! -z $pid ]]; then
+            echo -n ", killing hook with pid $pid"
+            kubectl exec --namespace=$NS $pod -- kill $pid
+            echo " - killed"
+        fi
+        sleep 2
+    done
+done
 
 declare -A finished_pvc
+echo -n "finished_pvc:0  pvcs:${#pvcs[@]} $statuses"
 while [[ ${#finished_pvc[@]} != ${#pvcs[@]} ]]; do
+    statuses=''
     for pvc in ${pvcs[@]}; do
-        status=$(kubectl get pvc $pvc --output=go-template --template='{{range .status.conditions}}{{.type}} {{end}}')
-        echo "status in $pvc - [$status]"
+        status=$(kubectl --namespace=$NS get pvc $pvc --output=go-template --template='{{range .status.conditions}}{{.type}} {{end}}')
+        statuses="$statuses $pvc:[$status]"
         if [[ $status == *FileSystemResizePending* ]]; then
             finished_pvc[$pvc]=1
         fi
         sleep 1
     done
-    echo "finished_pvc:${#finished_pvc[@]}  pvcs:${#pvcs[@]}"
+    echo -ne "\e[0K\rfinished_pvc:${#finished_pvc[@]} pvcs:${#pvcs[@]} - $statuses"
 done
+echo ""
 
 echo "scale back to ${#pods[@]}"
-kubectl scale statefulset $SS --replicas=${#pods[@]}
+kubectl scale statefulset --namespace=$NS $SS --replicas=${#pods[@]}
 
 running_pods=()
+echo -n "running_pods:0  expect:${#pods[@]}"
 while [[ ${#running_pods[@]} != ${#pods[@]} ]]; do
-    running_pods=(`kubectl get pod $selector -o go-template='{{range .items}}{{if eq .status.phase "Running"}}{{.metadata.name}} {{end}}{{end}}'`)
-    echo "running_pods:${#running_pods[@]}  expect:${#pods[@]}"
-    sleep 1
+    phases=`kubectl get pod --namespace=$NS $selector -o go-template='{{range .items}}{{.metadata.name}}:[{{.status.phase}}] {{end}}'`
+    running_pods=(`kubectl get pod --namespace=$NS $selector -o go-template='{{range .items}}{{if eq .status.phase "Running"}}{{.metadata.name}} {{end}}{{end}}'`)
+    echo -ne "\e[0K\rrunning_pods:${#running_pods[@]} expect:${#pods[@]} - $phases"
 done
+echo ""
 
-kubectl get pvc ${pvcs[@]}
+kubectl get pvc --namespace=$NS ${pvcs[@]}
